ee26cf92bacfd24ec0dd4554fa8295292862e91b
Dan Siegel <me@dansiegel.net>
2016-09-22 16:23:59 -0700

    updated assembly info

--- src/Prism.Plugin.Popups.Unity/Properties/AssemblyInfo.cs
+++ src/Prism.Plugin.Popups.Unity/Properties/AssemblyInfo.cs
@@ -1,30 +1,13 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Prism.Plugin.Popups.Unity")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prism.Plugin.Popups.Unity")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyTitle( "Prism.Plugin.Popups.Unity" )]
+[assembly: AssemblyDescription( "Prism Unity Plugin for Xamarin Forms to provide Popup Navigation" )]
+[assembly: AssemblyCompany( "AvantiPoint, LLC" )]
+[assembly: AssemblyProduct( "Prism.Plugin.Popups.Autofac" )]
+[assembly: AssemblyCopyright( "Copyright © Dan Siegel 2016" )]
+[assembly: NeutralResourcesLanguage( "en" )]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyInformationalVersion( "1.0.0-pre1" )]
