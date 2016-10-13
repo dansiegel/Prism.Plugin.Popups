9a15f65b8d2531e5a5d5859630808d7932038e93
Dan Siegel <me@dansiegel.net>
2016-10-13 14:18:59 -0700

    Updates AssemblyInfo

--- src/Prism.Plugin.Popups.Unity/Properties/AssemblyInfo.cs
+++ src/Prism.Plugin.Popups.Unity/Properties/AssemblyInfo.cs
@@ -1,13 +1,5 @@
 ﻿using System.Reflection;
-using System.Resources;
 
 [assembly: AssemblyTitle( "Prism.Plugin.Popups.Unity" )]
 [assembly: AssemblyDescription( "Prism Unity Plugin for Xamarin Forms to provide Popup Navigation" )]
-[assembly: AssemblyCompany( "AvantiPoint, LLC" )]
 [assembly: AssemblyProduct( "Prism.Plugin.Popups.Autofac" )]
-[assembly: AssemblyCopyright( "Copyright © Dan Siegel 2016" )]
-[assembly: NeutralResourcesLanguage( "en" )]
-
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
-[assembly: AssemblyInformationalVersion( "1.0.0-pre1" )]
