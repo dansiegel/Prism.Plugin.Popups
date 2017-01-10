a16bebd9326b4ce53548eb4a864f718e50591553
Dan Siegel <me@dansiegel.net>
2017-01-10 12:00:33 -0800

    Add Sample Project

---
+++ samples/PopupPluginSample/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using PopupPluginSample.Views;
+
+namespace PopupPluginSample
+{
+    public partial class App : PrismApplication
+    {
+        public App( IPlatformInitializer initializer = null ) : base( initializer ) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync( "MainPage?title=Hello%20from%20Xamarin.Forms" );
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+            Container.RegisterTypeForNavigation<PopupView>();
+        }
+    }
+}
+
---
+++ samples/PopupPluginSample/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace PopupPluginSample.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
