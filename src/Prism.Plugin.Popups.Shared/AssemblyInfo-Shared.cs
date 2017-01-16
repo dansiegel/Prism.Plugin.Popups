5b2a9bcd625c03f8a158312ad912efacac7b6046
Dan Siegel <me@dansiegel.net>
2017-01-16 10:12:07 -0800

    Updates Assembly Version to 1.1.0 for pre1 release

--- src/Prism.Plugin.Popups.Shared/AssemblyInfo-Shared.cs
+++ src/Prism.Plugin.Popups.Shared/AssemblyInfo-Shared.cs
@@ -5,6 +5,6 @@ using System.Resources;
 [assembly: AssemblyCopyright( "Copyright © Dan Siegel 2016" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
-[assembly: AssemblyVersion( "1.0.1.0" )]
-[assembly: AssemblyFileVersion( "1.0.1.0" )]
-[assembly: AssemblyInformationalVersion( "1.0.1" )]
+[assembly: AssemblyVersion( "1.1.0.0" )]
+[assembly: AssemblyFileVersion( "1.1.0.0" )]
+[assembly: AssemblyInformationalVersion( "1.1.0-pre1" )]
