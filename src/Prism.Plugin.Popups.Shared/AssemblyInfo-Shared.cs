c6af78e9aabac57d770c4d9df090ecd7110c18c2
Dan Siegel <me@dansiegel.net>
2017-02-14 09:24:30 -0800

    update assembly info for pre3

--- src/Prism.Plugin.Popups.Shared/AssemblyInfo-Shared.cs
+++ src/Prism.Plugin.Popups.Shared/AssemblyInfo-Shared.cs
@@ -2,9 +2,9 @@
 using System.Resources;
 
 [assembly: AssemblyCompany( "AvantiPoint, LLC" )]
-[assembly: AssemblyCopyright( "Copyright © Dan Siegel 2016" )]
+[assembly: AssemblyCopyright( "Copyright © Dan Siegel 2017" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
 [assembly: AssemblyVersion( "1.1.0.0" )]
 [assembly: AssemblyFileVersion( "1.1.0.0" )]
-[assembly: AssemblyInformationalVersion( "1.1.0-pre2" )]
+[assembly: AssemblyInformationalVersion( "1.1.0-pre3" )]
