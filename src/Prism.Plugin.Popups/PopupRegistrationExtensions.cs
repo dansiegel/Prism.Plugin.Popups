fbb63902fc84fd105861334233f45f0d4509de28
Dan Siegel <me@dansiegel.net>
2018-03-12 15:53:13 -0700

    fixing implementation for DryIoc

--- src/Prism.Plugin.Popups/PopupRegistrationExtensions.cs
+++ src/Prism.Plugin.Popups/PopupRegistrationExtensions.cs
@@ -17,23 +17,26 @@ namespace Prism.Plugin.Popups
             where TService : PopupPageNavigationService
         {
             containerRegistry.RegisterInstance<IPopupNavigation>(PopupNavigation.Instance);
-            containerRegistry.Register<INavigationService, TService>(PrismApplicationBase.NavigationServiceName);
             containerRegistry.RegisterSingleton<IPageBehaviorFactory, PopupPageBehaviorFactory>();
             if (IsDryIocContainer(containerRegistry))
+            {
                 containerRegistry.Register<INavigationService, TService>();
+            }
+
+            containerRegistry.Register<INavigationService, TService>(PrismApplicationBase.NavigationServiceName);
         }
 
         public static void RegisterPopupNavigationService(this IContainerRegistry containerRegistry) =>
             containerRegistry.RegisterPopupNavigationService<PopupPageNavigationService>();
 
-        public static bool IsDryIocContainer(IContainerRegistry containerRegistry)
+        private static bool IsDryIocContainer(IContainerRegistry containerRegistry)
         {
             var regType = containerRegistry.GetType();
             var propInfo = regType.GetRuntimeProperty("Instance");
 #if NETSTANDARD1_0
             return propInfo?.PropertyType.FullName.Equals("DryIoc.IContainer", StringComparison.OrdinalIgnoreCase) ?? false;
 #else
-			return propInfo?.PropertyType.FullName.Equals("DryIoc.IContainer", StringComparison.InvariantCultureIgnoreCase) ?? false;
+            return propInfo?.PropertyType.FullName.Equals("DryIoc.IContainer", StringComparison.InvariantCultureIgnoreCase) ?? false;
 #endif
         }
     }
